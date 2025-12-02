b18d7495fa6bf8d17a570783b3181165c354db33
ghcastellano <support@sootech.io>
2025-12-02 16:33:52 -0300

    Remove .vscode, scripts e plugin local niimbot

--- plugins/niimbot_label_printer/CHANGELOG.md
+++
@@ -1,8 +0,0 @@
-## 0.0.2
-
-* Add labelType to PrintData
-* Add image to example desing image of canvas
-
-## 0.0.1
-
-* Upload all files
--- plugins/niimbot_label_printer/example/android/app/src/main/kotlin/com/example/niimbot_label_printer_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.niimbot_label_printer_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
